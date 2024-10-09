6529b1058639f0a580fbd82bebd5fc1b04864305
HellerH <helleraccoust@gmail.com>
2024-10-09 15:46:09 +0000

    Error prediction

--- calculator.py
+++ calculator.py
@@ -1,26 +1,67 @@
-num_1: float = float(input('Enter first number: '))
-num_2: float = float(input('Enter second number: '))
+try:
+  num_1: float = float(input('Enter first number: '))
+except ValueError:
+    print('Please input a valid number')
+except NameError:
+    print('Please rerun program')
+try:
+   num_2: float = float(input('Enter second number: '))
+except ValueError:
+    print('Please input a valid number')
+except NameError:
+    print('Please restart program')
+
+
 operator: str = input('Enter operation symbol(+ , - , * , / , ** , // , % ): ')
+
+
 if operator == '+':
-    sum = num_1 + num_2
-    print(sum)
+    try:
+       sum = num_1 + num_2
+       print(sum)
+    except NameError:
+        print('Please rerun program')
 elif operator == '-':
-    sum = num_1 - num_2
-    print(sum)
+    try:
+       sum = num_1 - num_2
+       print(sum)
+    except NameError:
+        print('Please rerun program')
 elif operator == '*':
-    sum = num_1 * num_2
-    print(sum)
+    try:
+       sum = num_1 * num_2
+       print(sum)
+    except NameError:
+        print('Please rerun program')
 elif operator == '/':
-    sum = num_1 / num_2
-    print(sum)
+    try:
+       sum = num_1 / num_2
+       print(sum)
+    except ZeroDivisionError:
+        print('Division by zero not possible')
+    except NameError:
+        print('Please rerun program')
 elif operator == '**':
-    sum = num_1 ** num_2
-    print(sum)
+    try:
+       sum = num_1 ** num_2
+       print(sum)
+    except NameError:
+        print('Please rerun program')
 elif operator == '//':
-    sum = num_1 // num_2
-    print(sum)
+    try:
+      sum = num_1 // num_2
+      print(sum)
+    except ZeroDivisionError:
+        print('Division by zero not possible')
+    except NameError:
+        print('Please rerun program')
 elif operator == '%':
-    sum = num_1 % num_2
-    print(sum)
+    try:
+       sum = num_1 % num_2
+       print(sum)
+    except ZeroDivisionError:
+        print('Remainder not defined')
+    except NameError:
+        print('Please rerun program')
 else:
     print('Please input one operator from the given list')
