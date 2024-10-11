67307eb5c7fee60e0076e5a31b81fd66e97b2779
HellerH <helleraccoust@gmail.com>
2024-10-11 05:05:13 +0000

    movie recommendation system

--- hackathon.py
+++ hackathon.py
@@ -1,21 +1,59 @@
-from flask import Flask,redirect,url_for
+# from flask import Flask,redirect,url_for
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+
+
+# def home():
+#     return 'Hello world <h1>HELLO<h1>'
+
+# @app.route('/<name>')
+# def user(name):
+#     return f'Hello {name}'
+
+# @app.route('/admin')
+# def admin():
+#     return redirect(url_for('home'))
+
+# if __name__ == "__main__":
+#     app.run(debug = True)
+
+from flask import Flask, request, render_template
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
+#Load the Dataset
+df = pd.read_csv('tmdb_5000_movies.csv')
 
-@app.route("/")
+# Preprocess the data
+tfidf = TfidfVectorizer(stop_words='english')
+df['overview'] = df['overview'].fillna('')
+tfidf_matrix = tfidf .fit_transform(df['overview'])
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
+# Function to get recommendations
+def get_recomendations(title, cosine_sim=cosine_sim):
+    idx = df[df['title']] == [title].index[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse = True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    return df['title'].iloc[movie_indices]
 
+@app.route('/')
 def home():
-    return 'Hello world <h1>HELLO<h1>'
+    return render_template('index.html')
 
-@app.route('/<name>')
-def user(name):
-    return f'Hello {name}'
+@app.route('/recommend' , methods=['POST'])
+def recommend():
+    movie = request.form['movie']
+    recommendations = get_recomendations(movie)
+    return render_template('recommend.html', movie=movie, recommendations=recommendations)
 
-@app.route('/admin')
-def admin():
-    return redirect(url_for('home'))
-
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
